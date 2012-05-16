a8dd85848ea0dd1cbeef9f1af7107628939d9db3
Olivier Refalo <orefalo@yahoo.com>
2012-05-16 18:47:58 -0400

    rs upstream should abort

--- cmds/g2-rs.sh
+++ cmds/g2-rs.sh
@@ -4,6 +4,7 @@
 [ $1 == "upstream" ] && {
     read -p "warning: resetting to the upstream may erase any local changes, are you sure (y/n)? " -n 1 -r
     [[ $REPLY == [yY]* ]] && { echo
+        "$GIT_EXE" abort
         remote=$("$GIT_EXE" g2getremote)
         [[ -n $remote ]] && { echo "Resetting branch to $remote" && "$GIT_EXE" reset --hard "$remote"; } || echo "fatal: please setup tracking for this branch, see <track>";
     }
