0b787cfa5f4277ec0640b03dceff745e334a7c40
Olivier Refalo <orefalo@yahoo.com>
2012-06-02 10:00:24 -0400

    fix the upgrade

--- cmds/g2-g2upgrade.sh
+++ cmds/g2-g2upgrade.sh
@@ -4,8 +4,16 @@
 read -p "Do you really want to upgrade g2 (y/n)? " -n 1 -r
 echo
 [[ $REPLY = [yY]* ]] && {
-    cd /etc/g2
+
+    [[ -f /etc/g2/g2-install.sh ]] && RPATH="/etc/g2"
+    [[ -f ./g2-install.sh ]] && RPATH="."
+
+    [[ -z $RPATH ]] && echo "G2 not found, please cd to the install folder" && exit 1
+
+    cd "$RPATH"
     "$GIT_EXE" fetch
-    "$GIT_EXE" reset origin/master
+    g2excludes=$("$GIT_EXE" config --global --get g2.panic.excludes)
+    "$GIT_EXE" reset --hard origin/master && "$GIT_EXE clean -fdx $g2excludes"
+
     echo "Congratulations, please close this bash session and open a new one"
 }
