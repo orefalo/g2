e863f67470cdcbdd78c662269dd53a14c4c465f9
Olivier Refalo <orefalo@yahoo.com>
2012-06-05 16:04:03 -0400

    fix issue with rb/g2isrebasing

--- cmds/g2-g2isrebasing.sh
+++ cmds/g2-g2isrebasing.sh
@@ -3,4 +3,4 @@
 GDIR=$("$GIT_EXE" rev-parse --git-dir 2> /dev/null)
 
 [[ ! $GDIR ]] && echo "fatal: Not a Git Repository" && exit 1
-[[ -d "$GDIR/rebase-merge" -o -d "$GDIR/rebase-apply" ]] && echo "true" || echo "false"
+[[ -d "$GDIR/rebase-merge" || -d "$GDIR/rebase-apply" ]] && echo "true" || echo "false"
