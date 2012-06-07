b957f83bf9cf4d96d85841aa72507c70e3102c90
Olivier Refalo <orefalo@yahoo.com>
2012-06-07 16:21:02 -0400

    first undo version

--- cmds/g2-undo.sh
+++ cmds/g2-undo.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 #
 
-# undo file
-# undo commit
-# undo merge
+if [ $# -lt 1 ]
+then
+    echo "Usage : g undo <file|'commit'|'merge'>"
+    exit
+fi
+
+read -p "warning: the action will discard your changes, please confirm (y/n)? " -n 1 -r
+echo
+[[ $REPLY = [nN]* ]] && exit 0
+
+case "$1" in
+
+    "commit")
+        #TODO: Validate local commits
+        echo "Undoing last commit and reverting changes to the staging area."
+        "$GIT_EXE" reset --soft HEAD^
+    ;;
+    "merge")
+        #TODO: Validate local commits
+        echo "Reverting back prior to the last merge."
+        "$GIT_EXE" reset --hard ORIG_HEAD
+    ;;
+    *) "$GIT_EXE" checkout -- "$1"
+    ;;
+esac
