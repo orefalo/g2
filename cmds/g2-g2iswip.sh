0dd457901247c26f4c016f7a27a1aa10267ca164
Olivier Refalo <orefalo@yahoo.com>
2012-06-07 16:20:50 -0400

    fix commit error on bare repo

--- cmds/g2-g2iswip.sh
+++ cmds/g2-g2iswip.sh
@@ -3,5 +3,5 @@
 
 remote=$1
 [[ -z $1 ]] && remote=$("$GIT_EXE" g2getremote)
-[[ -z $remote ]] && [[ $("$GIT_EXE" log --oneline -1 | cut -f 2 -d " " | grep -c wip) -gt 0 ]] && echo "true" && exit 0
-[[ $("$GIT_EXE" log $remote..HEAD --oneline | cut -f 2 -d " " | uniq | grep -c wip) -gt 0 ]] && echo "true" || echo "false"
+[[ -z $remote ]] && [[ $("$GIT_EXE" log --oneline -1 2>/dev/null | cut -f 2 -d " " | grep -c wip) -gt 0 ]] && echo "true" && exit 0
+[[ $("$GIT_EXE" log $remote..HEAD --oneline 2>/dev/null | cut -f 2 -d " " | uniq | grep -c wip) -gt 0 ]] && echo "true" || echo "false"
