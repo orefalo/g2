e6939a19f044b322300001dfec56bc4a2dc144e9
Olivier Refalo <orefalo@yahoo.com>
2012-06-20 12:02:53 -0400

    fix bug, g rb was called an unknown alias

--- cmds/g2-rb.sh
+++ cmds/g2-rb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-[[ $("$GIT_EXE" g2isrebasing) = "false" ]] && {
+[[ $("$GIT_EXE" g2brstatus) = "false" ]] && {
     read -p "The history is about to be rewritten. This is a dangerous operation, please confirm (y/n)? " -n 1 -r
     echo
     [[ $REPLY = [nN]* ]] && exit 0
