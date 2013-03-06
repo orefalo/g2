3080e0632a5c8baf9068eace1abc0e9e45178e36
Olivier Refalo <orefalo@yahoo.com>
2013-03-05 23:54:40 -0500

    fix sync saying that branch is in the past

--- cmds/g2-g2isbehind.sh
+++ cmds/g2-g2isbehind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Returns 1 if the branch is behind its upstream branch, 0 if not
+# Returns 0 if the branch is behind its upstream branch, 1 if not
 
 local=$("$GIT_EXE" branch | grep "*" | sed "s/* //")
 remote=$1
@@ -8,6 +8,6 @@ remote=$1
 [[ -n $remote ]] && {
     "$GIT_EXE" fetch
     RIGHT_AHEAD=$("$GIT_EXE" rev-list --left-right ${local}...${remote} -- 2> /dev/null | grep -c "^>")
-    [[ $RIGHT_AHEAD -gt 0 ]] && exit 1;
+    [[ $RIGHT_AHEAD -gt 0 ]] && exit 0;
 }
-exit 0;
+exit 1;
