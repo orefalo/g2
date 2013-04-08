92a8d95d6fae7a476437290145c4e04d1bcb1715
Olivier Refalo <orefalo@yahoo.com>
2013-04-08 18:56:29 -0400

    allow g co <hash>

--- cmds/g2-co.sh
+++ cmds/g2-co.sh
@@ -3,15 +3,18 @@
 
 source "$G2_HOME/cmds/color.sh"
 
-isBranch=$("$GIT_EXE" branch -a | grep -c "$1")
-[[ $isBranch -gt 0 ]] && {
-    "$GIT_EXE" abort
-    $("$GIT_EXE" g2haschanges) || exit 1
-    g2excludes=$("$GIT_EXE" config --global --get g2.panic.excludes)
-    "$GIT_EXE" checkout "$@" && "$GIT_EXE" clean -fdx $g2excludes
-    exit $?;
-}
+if [ "$(echo "$1" | grep -e "^[0-9A-Fa-f]*$")" == "$1" ]; then
+    "$GIT_EXE" checkout $*
+else
+    isBranch=$("$GIT_EXE" branch -a | grep -c "$1")
+    [[ $isBranch -gt 0 ]] && {
+        "$GIT_EXE" abort
+        $("$GIT_EXE" g2haschanges) || exit 1
+        g2excludes=$("$GIT_EXE" config --global --get g2.panic.excludes)
+        "$GIT_EXE" checkout "$@" && "$GIT_EXE" clean -fdx $g2excludes
+        exit $?;
+    }
 
-echo_info "There is no branch named '$1', you may want to run 'g fetch <remote_name>' to refresh from the server"
-echo_info "If you are trying to revert a file, consider 'g undo <file>'"
-
+    echo_info "There is no branch named '$1', you may want to run 'g fetch <remote_name>' to refresh from the server"
+    echo_info "If you are trying to revert a file, consider 'g undo <file>'"
+fi
